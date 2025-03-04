eb5c828814a25e69435c5a70d0a1922db66a8106
sonyoon7 <eunice@spoonradio.co>
2025-03-04 21:46:01 +0900

    feat(build.gradle): reactor-netty

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,16 +10,23 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = uri("https://repo.spring.io/release") }
 }
-
 
 
 
 dependencies {
     implementation("io.projectreactor:reactor-core:3.7.3")
+    // import a BOM
+    implementation(platform("io.projectreactor:reactor-bom:2024.0.3"))
+
+    // define dependencies without versions
+    implementation("io.projectreactor.netty:reactor-netty-core")
+    implementation("io.projectreactor.netty:reactor-netty-http")
 
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
 }
 
 tasks.test {
