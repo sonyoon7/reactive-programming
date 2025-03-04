99e7c567030246d3f08ea042d9deae13fc3a0fcc
sonyoon7 <eunice@spoonradio.co>
2025-03-04 22:19:53 +0900

    feat: Reactor Netty TCP Server 구현
    -  nc localhost 8080 명령어 터미널에서 실행해보기

--- src/main/java/org/example/reactornetty/Application.java
+++ src/main/java/org/example/reactornetty/Application.java
@@ -1,7 +1,9 @@
 package org.example.reactornetty;
 
+import reactor.core.publisher.Mono;
 import reactor.netty.DisposableServer;
 import reactor.netty.tcp.TcpServer;
+import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 public class Application {
 
@@ -10,7 +12,20 @@ public class Application {
                 TcpServer.create()
                         .host("localhost")
                         .port(8080)
+                        .doOnConnection(conn -> System.out.println("New Connection: " + conn.address()))
+                        .doOnBind(serverConfig-> System.out.println("Server is starting..."))
+                        .doOnUnbound(disposableServer ->  System.out.println("Server is stopping..."))
+                        .wiretap("reactor.netty.tcp", io.netty.handler.logging.LogLevel.DEBUG, AdvancedByteBufFormat.TEXTUAL) // 로그 설정
+                        .handle((inbound, outbound) ->
+                                inbound.receive()
+                                        .asString()
+                                        .doOnNext(msg -> System.out.println("Received: " + msg))
+                                        .flatMap(msg -> outbound.sendString(
+                                                Mono.just("Echo: " + msg + "\n")))
+                                        .then()
+                        )
                         .bindNow();
+
 
         server.onDispose()
                 .block();
