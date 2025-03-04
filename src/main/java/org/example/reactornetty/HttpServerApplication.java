4f415ff9aa245bb21445263564dfc7064739778a
sonyoon7 <eunice@spoonradio.co>
2025-03-04 22:49:37 +0900

    (HttpServerApplication): route 구현

--- src/main/java/org/example/reactornetty/HttpServerApplication.java
+++ src/main/java/org/example/reactornetty/HttpServerApplication.java
@@ -3,8 +3,6 @@ package org.example.reactornetty;
 import reactor.core.publisher.Mono;
 import reactor.netty.DisposableServer;
 import reactor.netty.http.server.HttpServer;
-import reactor.netty.tcp.TcpServer;
-import reactor.netty.transport.logging.AdvancedByteBufFormat;
 
 public class HttpServerApplication {
 
@@ -13,6 +11,15 @@ public class HttpServerApplication {
                 HttpServer.create()
                         .host("localhost")
                         .port(8080)
+                        .route(routes ->
+                                routes.get("/hello",
+                                                (request, response) -> response.sendString(Mono.just("Hello World!")))
+                                        .post("/echo",
+                                                (request, response) -> response.send(request.receive().retain()))
+                                        .get("/path/{param}",
+                                                (request, response) -> response.sendString(Mono.just(request.param("param"))))
+                                        .ws("/ws",
+                                                (wsInbound, wsOutbound) -> wsOutbound.send(wsInbound.receive().retain())))
                         .bindNow();
 
         server.onDispose()
